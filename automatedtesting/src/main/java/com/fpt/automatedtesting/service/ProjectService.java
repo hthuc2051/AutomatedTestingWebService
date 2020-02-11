a53bf9cdf701ce40cc06ab4610febc5557104926
nhthuc1502 <nhthuc1502@gmail.com>
2020-02-11 09:10:04 +0700

    Remove unused service (Project)

--- automatedtesting/src/main/java/com/fpt/automatedtesting/service/ProjectService.java
+++
@@ -1,7 +0,0 @@
-package com.fpt.automatedtesting.service;
-
-import javax.servlet.http.HttpServletResponse;
-
-public interface ProjectService {
-    void downloadJavaConsoleApp(HttpServletResponse response, Integer scriptId);
-}
