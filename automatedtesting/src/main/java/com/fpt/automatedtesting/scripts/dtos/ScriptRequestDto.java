423e6e299c6893308afe162d8290cf6e3a009e3e
Nhat Thien <thienvo0211@gmail.com>
2020-04-18 16:11:17 +0700

    Save template question and database

--- automatedtesting/src/main/java/com/fpt/automatedtesting/scripts/dtos/ScriptRequestDto.java
+++ automatedtesting/src/main/java/com/fpt/automatedtesting/scripts/dtos/ScriptRequestDto.java
@@ -18,6 +18,8 @@ public class ScriptRequestDto {
     private Integer subjectId;
     private String scriptData;
     private MultipartFile docsFile;
-    private ConnectionObj onlineObj;
-    private ConnectionObj offlineObj;
+    private MultipartFile database;
+    private MultipartFile templateQuestion;
+    private String onlineConnection;
+    private String offlineConnection;
 }
