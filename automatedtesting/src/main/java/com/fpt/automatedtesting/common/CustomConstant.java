fe23cfff93dc63f0e4b574209956218f9149a195
Nhat Thien <thienvo0211@gmail.com>
2020-04-20 22:02:53 +0700

    change subject code

--- automatedtesting/src/main/java/com/fpt/automatedtesting/common/CustomConstant.java
+++ automatedtesting/src/main/java/com/fpt/automatedtesting/common/CustomConstant.java
@@ -10,10 +10,10 @@ public class CustomConstant {
 
 
     // Script type
-    public static final String TEMPLATE_TYPE_JAVA = "Java";
-    public static final String TEMPLATE_TYPE_JAVA_WEB = "JavaWeb";
-    public static final String TEMPLATE_TYPE_C = "C";
-    public static final String TEMPLATE_TYPE_CSHARP = "CSharp";
+    public static final String TEMPLATE_TYPE_JAVA = "PRO192";
+    public static final String TEMPLATE_TYPE_JAVA_WEB = "PRJ321";
+    public static final String TEMPLATE_TYPE_C = "PRF192";
+    public static final String TEMPLATE_TYPE_CSHARP = "PRN292";
     public static final String PRACTICAL_INFO_FILE_NAME = "practical-info.json";
 
     public static final String COLUMN_NO = "No";
